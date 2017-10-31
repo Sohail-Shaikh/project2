e4f4e674218888440ebb3413d8fa4fd43e037dd6
sooliShaikh <sohail.shaikh@annet.com>
2017-10-31 17:41:00 +0530

    remove home file

--- home.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('session_verify.php');
-?>
-<body>
-<h4>Welcome <?php echo $login_session; ?></h1>
-<h4><a href="logout.php">Logout</a></h4>
-</body>
-
-<!-- https://github.com/sooliShaikh/project2.git -->
